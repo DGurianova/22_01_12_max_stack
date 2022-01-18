8628a4664f4cadc1caaf804dfb20fd9f9665cee6
DGurianova <daria.gurianova1@gmail.com>
2022-01-18 22:41:09 +0100

    ImprovedMaxStack + NaiveMaxStack

--- src/com/company/NaiveMaxStack.java
+++ src/com/company/NaiveMaxStack.java
@@ -2,23 +2,14 @@ package com.company;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.EmptyStackException;
-import java.util.Stack;
 
 public class NaiveMaxStack implements MaxStack {
 
     private final Deque<Integer> source = new ArrayDeque<>();
-    Stack<Integer> sourceStackMax = new Stack<Integer>();
 
     @Override
     public void add(int elt) {
         source.addLast(elt);
-        if (sourceStackMax.size() == 0) {
-            throw new EmptyStackException();
-        }
-        if (sourceStackMax.peek() < elt) {
-            sourceStackMax.push(elt);
-        }
     }
 
     @Override
@@ -28,14 +19,7 @@ public class NaiveMaxStack implements MaxStack {
 
     @Override
     public int remove() {
-        int elt = source.removeLast();
-        if (sourceStackMax.size() == 0) {
-            throw new EmptyStackException();
-        }
-        if (elt == sourceStackMax.peek()) {
-            sourceStackMax.pop();
-        }
-        return elt;
+        return source.removeLast();
     }
 
     @Override
@@ -45,9 +29,12 @@ public class NaiveMaxStack implements MaxStack {
 
     @Override
     public int getMax() {
-        if (sourceStackMax.size() > 0) {
-            return sourceStackMax.peek();
+        int currentMax = source.getFirst();
+
+        for (int elt : source) {
+            if (elt > currentMax)
+                currentMax = elt;
         }
-        throw new EmptyStackException();
+        return currentMax;
     }
 }
